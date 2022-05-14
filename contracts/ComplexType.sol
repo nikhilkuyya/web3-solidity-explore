8e91de910e2101d562783214ac613f57dd242e74
nikhilkuyya <nikhilkuyya@gmail.com>
2022-05-14 19:37:36 +0530

    chore: update contract

--- contracts/ComplexType.sol
+++ contracts/ComplexType.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract StateVisibility {
+contract ComplexType {
 
     // we observe that we get the primitive value type as return value for the getters.
     string[][] public myweekTodos;
